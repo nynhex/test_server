1fe1b68889e4dc3c12b32845875c6d6f9170f05c
nynhex <nynhex@gmail.com>
2018-02-03 09:22:23 -0600

    Change stuff up

--- .chef/knife.rb
+++ .chef/knife.rb
@@ -2,6 +2,5 @@ node_name                'nynhex'
 client_key               '/Users/nynhex/Dropbox/test_server/.chef/nynhex.pem'
 validation_client_name   'chef-validator'
 validation_key           '/etc/chef-server/chef-validator.pem'
-chef_server_url          'https://lucifer.attlocal.net/organizations/myorg'
+chef_server_url          'https://localhost'
 syntax_check_cache_path  '/Users/nynhex/Dropbox/test_server/.chef/syntax_check_cache'
-cookbook_path [ './cookbooks' ]
